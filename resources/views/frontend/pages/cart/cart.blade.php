2a60af4c049838070be7970dfafc9912e37959e0
Md. Tarekul Islam <97617181+Tarek4521@users.noreply.github.com>
2022-02-18 00:03:42 +0600

    cart_done

---
+++ resources/views/frontend/pages/cart/cart.blade.php
@@ -0,0 +1,29 @@
+@extends('frontend.layouts.app')
+
+@section('content')
+
+<main class="main">
+    @include('frontend.pages.cart.section.page_header')
+   @include('frontend.pages.cart.section.breadcrumb')
+
+    <div class="page-content">
+        <div class="cart">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9">
+                      @include('frontend.pages.cart.section.table_wishlist')
+                        @include('frontend.pages.cart.section.cart_bottom')
+
+                    </div><!-- End .col-lg-9 -->
+                    <aside class="col-lg-3">
+                      @include('frontend.pages.cart.section.cart_summery')
+
+                        <a href="category.html" class="btn btn-outline-dark-2 btn-block mb-3"><span>CONTINUE SHOPPING</span><i class="icon-refresh"></i></a>
+                    </aside><!-- End .col-lg-3 -->
+                </div><!-- End .row -->
+            </div><!-- End .container -->
+        </div><!-- End .cart -->
+    </div><!-- End .page-content -->
+</main><!-- End .main -->
+
+@endsection
